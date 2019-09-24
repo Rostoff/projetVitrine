e8177b00447d6f1ab2e2f6f4e57f68de68e06804
christophe <christophe.couchy@gmail.com>
2019-09-24 21:21:58 +0200

    Creation du projet

---
+++ vitrine/src/main/java/com/rostoff/projetvitrine/ProjetVitrineApplication.java
@@ -0,0 +1,13 @@
+package com.rostoff.projetvitrine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetVitrineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetVitrineApplication.class, args);
+    }
+
+}
